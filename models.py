9ef6c62c9f03d1fb59341a1601af2f74fc0c497f
junhyuk000 <junhyuk000@gmail.com>
2025-02-07 14:56:42 +0900

    영화 키 재발급

--- models.py
+++ models.py
@@ -452,7 +452,7 @@ class DBManager:
 
     ### KOBIS사이트에서 일별 박스오피스 및 영화 상세정보 API 가져와서 PANDAS를 활용하여 필요한 데이터 추출    
     def moives_info(self):
-        servicekey = '7073050047b5b1695c8bb456bc61d310'
+        servicekey = 'b746e25d8c4fbffa4459e355269df3ea'
         today = datetime.now()
         yesterday = today - timedelta(days=1)
         yesterday_formatted = yesterday.strftime('%Y%m%d')
