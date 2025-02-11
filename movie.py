088772c2cf0ff761a5d460071310c2c3d4e0af9e
junhyuk000 <junhyuk000@gmail.com>
2025-02-11 18:25:59 +0900

    edit_post 수정

--- movie.py
+++ movie.py
@@ -283,7 +283,7 @@ def edit_post(movie_title, id):
         # 게시글 정보를 업데이트
         if manager.update_post(id, title, content, filename):
             flash("업데이트 성공!", "success")
-            return redirect(request.referrer or url_for('view_post', movie_title = title, id=id))  # 성공 시 메인 페이지로 리디렉션
+            return render_template('movie_view.html', movie_title = title, id=id))  # 성공 시 메인 페이지로 리디렉션
         return flash("게시글 수정 실패,400", 'error')  # 실패 시 400 에러 반환
 
     # GET 요청: 게시글 정보를 가져와 폼에 표시
